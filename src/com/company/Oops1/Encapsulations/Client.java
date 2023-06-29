af5da068c6b9d9f612fffdb6723eff07b76a0330
aakashbhatt <aakashbhatt.19@gmail.com>
2023-06-29 21:13:28 +0530

    modify

--- src/com/company/Oops1/Encapsulations/Client.java
+++ src/com/company/Oops1/Encapsulations/Client.java
@@ -4,6 +4,7 @@ public class Client {
     public static void main(String[] args) {
         Student st = new Student();
         st.setAge(23);
+//        st.age = 10; // which is voilating encapsulations
         st.setName("Aakash");
         st.display();
     }
