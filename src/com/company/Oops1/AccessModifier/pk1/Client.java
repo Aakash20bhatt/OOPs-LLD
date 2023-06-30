fe089d8552164de45fc25ff4481868aa920a7322
aakashbhatt <aakashbhatt.19@gmail.com>
2023-06-30 23:52:14 +0530

    accessModifier

---
+++ src/com/company/Oops1/AccessModifier/pk1/Client.java
@@ -0,0 +1,10 @@
+package com.company.Oops1.AccessModifier.pk1;
+
+public class Client {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.d1 = 10;
+        p.d2 = 20;
+//        p.d3 = 30;// Error
+    }
+}
---
+++ src/com/company/Oops1/AccessModifier/pk2/Client.java
@@ -0,0 +1,12 @@
+package com.company.Oops1.AccessModifier.pk2;
+
+import com.company.Oops1.AccessModifier.pk1.Person;
+
+public class Client {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.d1=100;
+//        p.d2 = 200;//Error
+//        p.d3 = 300 Error
+    }
+}
