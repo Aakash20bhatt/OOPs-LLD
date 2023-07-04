9c58c74b27857e981ec1c8e282bad41bffdd1314
aakashbhatt <aakashbhatt.19@gmail.com>
2023-07-04 16:56:11 +0530

    final keyword

---
+++ src/com/company/Oops/Final/Client.java
@@ -0,0 +1,15 @@
+package com.company.Oops.Final;
+
+public class Client {
+    public static void main(String[] args) {
+        final int x = 10;
+//        x=100;Error
+
+        System.out.println(x);
+
+        DemoClass d = new DemoClass();
+//        d.x = 100; Error
+        System.out.println(d.x);
+        System.out.println(d.y);
+    }
+}
---
+++ src/com/company/Oops/Final/DemoClass.java
@@ -0,0 +1,12 @@
+package com.company.Oops.Final;
+
+public class DemoClass {
+    // In final keyword, initialized at declaration, or the constructor only.
+    final int x = 10;
+    final int y ;
+
+    DemoClass(){
+        y=20;
+    }
+
+}
